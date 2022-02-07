9d59aa19f22ea5bbe82a31294a271a58c0978021
mmedranom <mmedranom@unal.edu.co>
2022-02-06 23:50:07 -0500

    sos

--- content/sketches/photomosaic3.js
+++ content/sketches/photomosaic3.js
@@ -9,20 +9,12 @@ function preload() {
     img=loadImage('/vc/sketches/lenna.png')
 
     //carga de iamgtenes que haran parte del shader
-    alpha0 = loadImage('/vc/sketches/amarillo.jpg')
-    alpha1 = loadImage('/vc/sketches/azul.jpg')
-    alpha2 = loadImage('/vc/sketches/blanco.jpg')
-    alpha3 = loadImage('/vc/sketches/cyan.jpg')
-    alpha4 = loadImage('/vc/sketches/morado.jpg')
-    alpha5 = loadImage('/vc/sketches/naranja.jpg')
-    alpha6 = loadImage('/vc/sketches/negro.jpg')
-    alpha7 = loadImage('/vc/sketches/rojo.jpg')
-    alpha8 = loadImage('/vc/sketches/rosado.jpg')
-    alpha9 = loadImage('/vc/sketches/verde.jpg')
-    alpha10 = loadImage('/vc/sketches/gris.jpg')
-    alpha11 = loadImage('/vc/sketches/cafe.jpg')
-    alpha12 = loadImage('/vc/sketches/fucsia.jpg')
-    images=[alpha0,alpha1,alpha2,alpha3,alpha4,alpha5,alpha6,alpha7,alpha8,alpha9,alpha10,alpha11,alpha12]
+    images = []
+    for (let i = 1; i <= 13; i++) {
+      console.log(images);
+      images.push(loadImage(`/vc/sketches/images/p${i}.jpg`));
+
+    }
 
     nImages=images.length;
     myShader = loadShader("/vc/sketches/shader.vert", "/vc/sketches/shader4.frag")
@@ -35,6 +27,7 @@ function compare(a,b){
   return a[0]-b[0];
 }
 function setup() {
+  //Se definen las dimensiones del lienzo
   enable_shader = createCheckbox('enable video', false);
   enable_shader.style('color', 'magenta');
   enable_shader.changed(() => {
@@ -44,9 +37,6 @@ function setup() {
     } else {
       console.log("no Checked")
       myShader.setUniform("texture", img);
-
-
-
     }
   });
   enable_shader.position(10, 50);
@@ -60,27 +50,9 @@ function setup() {
 
   myShader.setUniform("texture", img);
   myShader.setUniform("nImages", float(nImages));
-  console.log(alpha0.width,alpha0.height);
-  sortedImages=[]
-  for(let ind=0;ind<images.length;ind++){  
-    let score=0;
-    let cnt=0;
-    for(let i=0;i<images[ind].width;i+=5)
-      for(let j=0;j<images[ind].height;j+=5){
-        score+=0.333*images[ind].get(i,j)[0]+0.333*images[ind].get(i,j)[1]+0.333*images[ind].get(i,j)[2];
-        //score+=0.299*images[ind].get(i,j)[0]+0.587*images[ind].get(i,j)[1]+0.114*images[ind].get(i,j)[2];
-
-        cnt++;
-      }
-    score=score/(cnt);
-  
-    sortedImages.push([score,ind]);
-  }
-  sortedImages=sortedImages.sort(compare);
-
 
   for(let i = 0 ;i < nImages ;i++)
-    myShader.setUniform("alpha"+i.toString(), images[sortedImages[i][1]]);
+    myShader.setUniform("alpha"+i.toString(), images[i]);
   
   slider = createSlider(2, 16, 40);
   slider.position(10, 10);
